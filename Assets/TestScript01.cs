175c62576847447c7deaeb776aba63ff27fa4259
joemag <joemagiv@gmail.com>
2016-06-22 00:32:16 -0700

    Test commit, trying to commit in TestScript01

---
+++ Assets/TestScript01.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScript01 : MonoBehaviour {
+
+	//Testing out some stuff for VersionControl
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
