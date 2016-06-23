49797167643bf32fc547d489ed93f6b458290812
Joseph Magdalena <joemagiv@gmail.com>
2016-06-22 23:31:52 -0700

    Commit from Macbook. Minor script change.

--- Assets/TestScript02.cs
+++ Assets/TestScript02.cs
@@ -5,6 +5,8 @@ public class TestScript02 : MonoBehaviour {
 
 	//More testing of source control!
 
+	//Making more changes, this one came from my mac.
+
 	// Use this for initialization
 	void Start () {
 	
