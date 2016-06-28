b391bcefd8f14abd7684cd399c82001e8e5387bc
joemag <joemagiv@gmail.com>
2016-06-28 13:25:10 -0700

    Added Standard Assets and PlayerPrefs scripts

---
+++ Assets/Camera/CameraPan.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPan : MonoBehaviour {
+
+	private GameObject playerObject;
+
+	// Use this for initialization
+	void Start () {
+		playerObject = GameObject.FindGameObjectWithTag("Player");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (playerObject.transform.position);
+	}
+}
---
+++ Assets/GameScripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class GameManager : MonoBehaviour {
+
+	public bool recording = true;
+
+	// Use this for initialization
+	void Start () {
+		PlayerPrefsManager.UnlockLevel (2);
+		print (PlayerPrefsManager.IsLevelUnlocked (1));
+		print (PlayerPrefsManager.IsLevelUnlocked (2));
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (CrossPlatformInputManager.GetButton("Fire1")){
+			recording = false;
+		}else {
+			recording = true;
+		}
+	
+	}
+}
